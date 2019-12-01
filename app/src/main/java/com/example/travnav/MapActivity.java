9cefd447e63986f70783edd61a673d9093711658
abhinavsingh153 <abhinavsingh153@gmail.com>
2019-12-02 01:30:11 +0530

    Edit Splash Screen java

--- app/src/main/java/com/example/travnav/MapActivity.java
+++ app/src/main/java/com/example/travnav/MapActivity.java
@@ -110,10 +110,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
     }
 
     private void getLocationPermission(){
